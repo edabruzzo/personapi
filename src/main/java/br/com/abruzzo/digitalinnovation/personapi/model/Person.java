953c734f5de8100323b0d7653617206e87d678a7
Emmanuel Abruzzo <emmanuel.oliveira3@gmail.com>
2022-01-26 10:54:46 -0300

    Cria Entidade JPA Person

---
+++ src/main/java/br/com/abruzzo/digitalinnovation/personapi/model/Person.java
@@ -0,0 +1,54 @@
+package br.com.abruzzo.digitalinnovation.personapi.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "persons", schema = "public")
+public class Person {
+
+    @Id
+    @GeneratedValue
+    private UUID uuid;
+
+    @NonNull
+    @Column(nullable = false)
+    private String firstName;
+
+    @NonNull
+    @Column(nullable = false)
+    private String lastName;
+
+    @Pattern(regexp="(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    @Pattern(regexp="([0-9]{2}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[\\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[-]?[0-9]{2})\n")
+    private String cpf;
+
+    @Column(name="rg", nullable = false, unique = true)
+    @Pattern(regexp="(^\\d{1,2}).?(\\d{3}).?(\\d{3})-?(\\d{1}|X|x$)")
+    private String rg;
+
+    @NonNull
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    private List<Phone> phones = new ArrayList<>();
+
+
+
+
+}
