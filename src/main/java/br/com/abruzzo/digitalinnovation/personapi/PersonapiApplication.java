18144e06d6274c9f6252fda2c3757659f4db79f9
Emmanuel Abruzzo <emmanuel.oliveira3@gmail.com>
2022-01-27 07:51:17 -0300

    Adiciona ModelMapper para conversão entidade JPA para DTO (data transfer object), a fim de não expor entidades de domínio

--- src/main/java/br/com/abruzzo/digitalinnovation/personapi/PersonapiApplication.java
+++ src/main/java/br/com/abruzzo/digitalinnovation/personapi/PersonapiApplication.java
@@ -1,7 +1,9 @@
 package br.com.abruzzo.digitalinnovation.personapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PersonapiApplication {
@@ -10,4 +12,12 @@ public class PersonapiApplication {
 		SpringApplication.run(PersonapiApplication.class, args);
 	}
 
+
+	@Bean
+	public ModelMapper modelMapper () {return new ModelMapper();}
+
+
 }
+
+
+
