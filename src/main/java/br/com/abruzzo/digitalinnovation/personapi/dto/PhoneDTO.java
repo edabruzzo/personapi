ce28c37c9a8b8b4e1ee09e8c70373065a2aa003e
Emmanuel Abruzzo <emmanuel.oliveira3@gmail.com>
2022-01-28 09:16:20 -0300

    Implementa validações no DTO Phone

--- src/main/java/br/com/abruzzo/digitalinnovation/personapi/dto/PhoneDTO.java
+++ src/main/java/br/com/abruzzo/digitalinnovation/personapi/dto/PhoneDTO.java
@@ -1,14 +1,29 @@
 package br.com.abruzzo.digitalinnovation.personapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PhoneDTO {
 
     private UUID uuid;
+
+    @Enumerated(EnumType.STRING)
     private PhoneTypeDTO phoneTypeDTO;
+
+    @NotEmpty
+    @Size(min=10,max=14)
     private String number;
 
 
