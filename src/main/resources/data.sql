165dbf4b8bfd5aef7f58a48fade177d80cf55385
Emmanuel Abruzzo <emmanuel.oliveira3@gmail.com>
2022-01-28 09:12:01 -0300

    Desabilita script de inicialização de dados no banco

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO persons
-select (select nextval('hibernate_sequence')), 'Maria','Silva', 'maria@gmail.com', '333.333.333-33', '33.333.333-3', '1983-03-03';
-
-INSERT INTO persons
-select (select nextval('hibernate_sequence')), 'João','Santos', 'joao@gmail.com', '222.222.222-22', '22.222.222-2', '1982-02-02';
