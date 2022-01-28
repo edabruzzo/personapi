bbd4a65f8f1b81fb6d8c58968abb444000f183e6
Emmanuel Abruzzo <emmanuel.oliveira3@gmail.com>
2022-01-28 09:13:34 -0300

    Implementa método GET na API e altera URL base

--- src/main/java/br/com/abruzzo/digitalinnovation/personapi/controllers/PersonController.java
+++ src/main/java/br/com/abruzzo/digitalinnovation/personapi/controllers/PersonController.java
@@ -2,17 +2,19 @@ package br.com.abruzzo.digitalinnovation.personapi.controllers;
 
 
 import br.com.abruzzo.digitalinnovation.personapi.dto.PersonDTO;
+import br.com.abruzzo.digitalinnovation.personapi.exceptions.PersonDTONotFoundException;
 import br.com.abruzzo.digitalinnovation.personapi.model.Person;
 import br.com.abruzzo.digitalinnovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/people")
+@RequestMapping("/api/v1/persons")
 public class PersonController {
 
 
@@ -20,7 +22,20 @@ public class PersonController {
     private PersonService personService;
 
 
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<PersonDTO>> getAllPersonDTOs() {
+        List<PersonDTO> persons = this.personService.getAllPersonDTOs();
+        if (persons.isEmpty()) {
+            throw new PersonDTONotFoundException("Persons not found");
+        }
+        return new ResponseEntity<List<PersonDTO>>(persons, HttpStatus.OK);
+    }
+
+
+
+
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<PersonDTO> createPerson(@RequestBody PersonDTO personDTO){
         PersonDTO personCreated = this.personService.createPerson(personDTO);
         return ResponseEntity.ok(personCreated);
