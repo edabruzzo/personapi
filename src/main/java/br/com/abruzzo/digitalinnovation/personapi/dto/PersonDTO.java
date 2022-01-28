dd3a1b4184c13cfe7e15faf08faff9fd102e09e0
Emmanuel Abruzzo <emmanuel.oliveira3@gmail.com>
2022-01-28 09:15:00 -0300

    Implementa validações no DTO

--- src/main/java/br/com/abruzzo/digitalinnovation/personapi/dto/PersonDTO.java
+++ src/main/java/br/com/abruzzo/digitalinnovation/personapi/dto/PersonDTO.java
@@ -1,12 +1,12 @@
 package br.com.abruzzo.digitalinnovation.personapi.dto;
 
 import br.com.abruzzo.digitalinnovation.personapi.model.Phone;
-import lombok.Data;
-import lombok.NonNull;
+import lombok.*;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -14,15 +14,31 @@ import java.util.List;
 import java.util.UUID;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PersonDTO {
 
     private UUID uuid;
+
+    @NotEmpty
     private String firstName;
+
+    @NotEmpty
     private String lastName;
+
     private String email;
+
+    @NonNull
+    @Column(unique = true)
     private String cpf;
+
+    @NonNull
+    @Column(unique = true)
     private String rg;
+
     private LocalDate birthDate;
+
     private List<PhoneDTO> phonesDTO = new ArrayList<>();
 
 
